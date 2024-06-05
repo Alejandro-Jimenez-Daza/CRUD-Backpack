fb4fdbc55bf425190956a7accff8c99cfdcff642
Alejandro <alejodaza061@gmail.com>
2024-06-05 16:37:33 -0500

    Subo el proyecto CRUD a GitHub

---
+++ app/Models/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use CrudTrait;
+    use HasFactory;
+    protected $fillable = ['nombres', 'apellidos', 'grupo', 'edad'];
+}
---
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -0,0 +1,5 @@
+{{-- This file is used for menu items by any Backpack v6 theme --}}
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+<x-backpack::menu-item title="Posts" icon="la la-question" :link="backpack_url('post')" />
+<x-backpack::menu-item title="Users" icon="la la-question" :link="backpack_url('user')" />
